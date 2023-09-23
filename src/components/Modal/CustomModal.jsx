e25e55ebef72285f08846a405fa9016377e7b4c4
Daeeun Ko <7273322@naver.com>
2023-09-23 16:02:22 +0900

    feat: 참여자 정보 구현 - 데이터 저장 제외

--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -1,4 +1,5 @@
 import { Button, Modal } from '@goorm-dev/gds-challenge';
+import Modal1 from "@/components/Modal/Info/Modal1";
 
 const CustomModal = ({ isOpen, toggle }) => {
 	return (
@@ -6,7 +7,9 @@ const CustomModal = ({ isOpen, toggle }) => {
 			<Button size="lg" onClick={toggle}>설문조사 참여하기</Button>
 			<Modal isOpen={isOpen} toggle={toggle}>
 				<Modal.Header toggle={toggle}>헤더</Modal.Header>
-				<Modal.Body>컨텐츠를 넣어주세요.</Modal.Body>
+				<Modal.Body>
+					<Modal1 />
+				</Modal.Body>
 				<Modal.Footer>
 					<Button>다음</Button>
 				</Modal.Footer>
